07ab5b94177b6092cea6dac1182d1f6e22428f69
lufer <lufer@ipca.pt>
2023-10-25 11:45:51 +0100

    Bibliotecas - dll

---
+++ Aula 7 - DLL/GerePessoas/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * 
+ **/
+
+using System;
+using UmaPessoa;    //Biblioteca externa
+
+namespace GerePessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa(12, "Maria"); //usa Classe externa
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
